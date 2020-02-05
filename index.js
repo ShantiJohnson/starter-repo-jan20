a35cc5f457776815b230ff0d8abd32c3c6fc218e
Shanti <shanti_johnson@yahoo.com>
2020-02-05 16:51:50 -0600

    add excercise from class on variables.

---
+++ index.js
@@ -0,0 +1,26 @@
+// let timeOfDay = "evening!";
+
+// let greeting = `Well hello and good ${timeOfDay}`;
+
+// // console.log(greeting);
+
+// const bedTime = null;
+
+// console.log(greeting);
+
+const name = "Shanti";
+let favoriteSong = "Shiny by Jemaine Clement";
+let wage = 90000;
+let age = 27;
+const onlyChild = false;
+let satisfied = false;
+let favoriteThing = "My tattoo";
+// console.log(typeof favoriteThing);
+
+favoriteSong = "I get knocked down by Chumbawamba";
+favoriteThing = 2020;
+// console.log(typeof favoriteThing);
+age = 28;
+wage = 100000;
+satisfied = true;
+console.log("My name is " + name + " and I am not " + age + " years old!");
