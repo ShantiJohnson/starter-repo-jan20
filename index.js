69bad24e86ebe3841e5e1beaf67bc3a4b83b706f
Shanti <shanti_johnson@yahoo.com>
2020-02-07 20:18:22 -0600

    practice variables in class

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
-// let timeOfDay = "evening!";
-
-// let greeting = `Well hello and good ${timeOfDay}`;
-
-// // console.log(greeting);
-
-// const bedTime = null;
-
-// console.log(greeting);
-
 const name = "Shanti";
-let favoriteSong = "Shiny by Jemaine Clement";
-let wage = 90000;
+let favoriteSong = "The Box by Roddi Rich";
+let wage = 60000;
 let age = 27;
 const onlyChild = false;
 let satisfied = false;
-let favoriteThing = "My tattoo";
-// console.log(typeof favoriteThing);
-
-favoriteSong = "I get knocked down by Chumbawamba";
-favoriteThing = 2020;
-// console.log(typeof favoriteThing);
+let favoriteThing = "Cellphone";
+favoriteSong = "Set The Atmosphere y Kirk Carr";
+favoriteThing = 4;
+// console.log(typeof favoriteThing)
 age = 28;
 wage = 100000;
 satisfied = true;
-// console.log("My name is " + name + " and I am not " + age + " years old!");
-
-// console.log("2" + 2);
-// console.log(2 + "2");
-// console.log(2 + 2);
-// console.log(true + true);
-// console.log(true + true + true);
-// console.log(true + true + true + false);
-// console.log(false + true + true + true);
-// console.log(false + false + false);
-// console.log(2 + true);
-// console.log(2 + false);
-// console.log(true + true + " true" + "false");
-// console.log(1 + 1 + "2" + "3");
-// console.log(Number("2") + 2);
-
-// //let num = "11";
-// //console.log(Number(`${num}`) + 2);
+console.log(`My name is ${name} and I am not ${age}`);
